95130f6fd4380b14f6f6fc7ee5e771fd21f51cd1
Milan Ranpariya <ranpariya04@gmail.com>
2024-12-08 23:54:19 -0500

    Render all charts in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import ScatterChart from "./components/ScatterChart";
+import BubbleChart from "./components/BubbleChart";
 
 const App = () => {
   const [chartData, setChartData] = useState(null);
@@ -9,6 +13,20 @@ const App = () => {
       .then((data) => setChartData(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
+
+  if (!chartData) {
+    return <div>Loading data...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Financial Dashboard</h1>
+      <BarChart data={chartData} />
+      <LineChart data={chartData} />
+      <ScatterChart data={chartData} />
+      <BubbleChart data={chartData} />
+    </div>
+  );
 };
 
 export default App;
